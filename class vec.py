a1e8bd02806bed6030e2e95547f650e6963dbea9
ayaan261-goat <ktm.ayaangupta@gmail.com>
2025-05-18 09:03:41 +0545

    hi

---
+++ class vec.py
@@ -0,0 +1,14 @@
+class Vehicle:
+
+
+    def __init__(self, max_speed, mileage):
+        
+
+        self.max_speed = max_speed
+        self.mileage =mileage
+
+modelX= Vehicle(240, 18)
+
+print("model max")
+print("model max speed:",modelX.max_speed)
+print("model Mileage",modelX.mileage)
